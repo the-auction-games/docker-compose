34a1fe7a4d26f55fd1c53da696890f6fb3b04d57
Joel Smith <joelsmith.2019@gmail.com>
2023-03-02 12:34:28 -0700

    Add mongo image, update compose

--- databases/auction-mongo/restore.sh
+++ databases/auction-mongo/restore.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Waiting for MongoDB to start..."
-while ! nc -z localhost 27017; do
-  sleep 1
-done
-echo "MongoDB started"
+# echo "Waiting for MongoDB to start..."
+# while ! nc -z localhost 27017; do
+#   echo "Sleeping 1 sec..."
+#   sleep 1
+# done
+# echo "MongoDB started"
 
+sleep 10
 mongorestore dump/
